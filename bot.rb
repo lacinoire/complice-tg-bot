5e04bd367c4b02e70837707e7dbfc9b69ede3202
Carolin Brandt <carolin.brandt@tum.de>
2019-08-17 18:39:06 +0200

    various refactorings and rescuing from exceptions

--- bot.rb
+++ bot.rb
@@ -15,63 +15,73 @@ def split_command_botname(command)
   [raw_command, bot_name]
 end
 
+# determine wether we process further, command and text separation
+def preprocess_message(message, bot)
+  # ignore if message is not from you
+  return [false, '', ''] if message.from.id != Config.config['user']['tg_id']
+
+  command_data = message.entities.find { |entity| entity.type == 'bot_command'}
+  return [false, '', ''] if command_data.nil? # message to bot without command
+
+  if command_data.offset != 0
+    bot.api.send_message(chat_id: message.chat.id, text: 'Please give bot command first in your message :)')
+    return [false, '', '']
+  end
+
+  long_command = message.text[0, command_data.length]
+  command, bot_name = split_command_botname(long_command)
+  text = message.text[command_data.length + 1, message.text.length]
+
+  # ignore message cause it was adressed to another bot
+  return [false, '', ''] if !bot_name.nil? && bot_name != Config.config['bot']['username']
+
+  [true, command, text]
+end
+
 if $PROGRAM_NAME == __FILE__
 
   Telegram::Bot::Client.run(Config.config['bot']['token'], logger: Logger.new($stderr)) do |bot|
     bot.logger.info('Bot has been started')
     bot.listen do |message|
 
-      # ignore if message is not from you
-      next if message.from.id != Config.config['user']['tg_id']
+      begin
 
-      command_data = message.entities.find { |entity| entity.type == 'bot_command'}
-      next if command_data.nil? # message to bot without command
+        continue_processing, command, text = preprocess_message(message, bot)
+        next unless continue_processing
 
-      if command_data.offset != 0
-        bot.api.send_message(chat_id: message.chat.id, text: 'Please give bot command first in your message :)')
-        next
-      end
-
-      long_command = message.text[0, command_data.length]
-      command, bot_name = split_command_botname(long_command)
-
-      text = message.text[command_data.length + 1, message.text.length]
-
-      if !bot_name.nil? && bot_name != Config.config['bot']['username']
-        # ignore message cause it was adressed to another bot
-        next
-      end
-
-      case command
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-      when '/stop'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-      when '/compliceuser'
-        response = Complice.userinfo
-        bot.api.send_message(chat_id: message.chat.id, text: "#{response}")
-      when '/goals'
-        response = Complice.goals
-        bot.api.send_message(chat_id: message.chat.id, text: "#{response}")
-      when '/today'
-        response = Complice.today_full
-        bot.api.send_message(chat_id: message.chat.id, text: "#{response}")
-      when '/intention'
-        response = Complice.add_new_intention(text)
-        if response == 'wrong intention format'
-          bot.api.send_message(chat_id: message.chat.id, text: "Please use the intention format to add intentions!")
+        reply = 'default reply'
+        case command
+        when '/start'
+          reply = "Hello, #{message.from.first_name}"
+        when '/stop'
+          reply = "Bye, #{message.from.first_name}"
+        when '/compliceuser'
+          reply = Complice.userinfo
+        when '/goals'
+          reply = Complice.goals
+        when '/today'
+          reply = Complice.today_full
+        when '/intention'
+          response = Complice.add_new_intention(text)
+          reply = if response == 'wrong intention format'
+                    'Please use the intention format to add intentions!'
+                  else
+                    "You now have #{response} intentions for /today"
+                  end
+        when '/complete'
+          successful = Complice.complete(text)
+          reply = if successful
+                    "Item #{text} completed!"
+                  else
+                    'Please give a valid zid to complete!'
+                  end
         else
-          bot.api.send_message(chat_id: message.chat.id, text: "You now have #{response} intentions for /today")
+          reply = "I don't understand you :("
         end
-      when '/complete'
-        successful = Complice.complete(text)
-        if successful
-          bot.api.send_message(chat_id: message.chat.id, text: "Item #{text} completed!")
-        else
-          bot.api.send_message(chat_id: message.chat.id, text: "Please give a valid zid to complete!")
-        end
-      else
-        bot.api.send_message(chat_id: message.chat.id, text: "I don't understand you :(")
+        reply = 'reply from complice was empty' if reply.empty?
+        bot.api.send_message(chat_id: message.chat.id, text: reply)
+      rescue Exception => ex
+        bot.logger.info("Caugth exception: #{ex}")
       end
     end
   end
